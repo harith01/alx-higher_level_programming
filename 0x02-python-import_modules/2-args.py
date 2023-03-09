3bfbbc6563472f66e0d88e5a7cd69c709416942c
harith01 <harithyusuph@gmail.com>
2023-03-09 12:44:26 +0000

    task 2correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print("{} argument.".format(0))
+        print("{:d} argument.".format(0))
     elif len(argv) == 2:
-        print("{} argument:".format(1))
-        print("{}: {}".format(1, argv[1]))
+        print("{:d} argument:".format(1))
+        print("{:d}: {:s}".format(1, argv[1]))
     elif len(argv) > 2:
-        print("{} arguments:".format(len(argv) - 1))
+        print("{:d} arguments:".format(len(argv) - 1))
         for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+            print("{:d}: {:s}".format(i, argv[i]))
