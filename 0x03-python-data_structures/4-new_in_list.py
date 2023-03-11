3e492a5f81d79fcdf6850bfb4875b670f30d87f9
harith01 <harithyusuph@gmail.com>
2023-03-11 13:59:11 +0000

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if not my_list:
+        return None
+    new_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return new_list
+    for i in range(len(my_list)):
+        if (i == idx):
+            new_list[i] = element
+    return new_list
