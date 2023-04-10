f715050d128f49c87f5bbdbd62ae31afad0703c4
harith01 <harithyusuph@gmail.com>
2023-04-10 13:40:45 +0000

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -59,7 +59,8 @@ class Rectangle:
 
     def __str__(self):
         """Prints the rectangle with # character"""
-
+        if self.width == 0 or self.height == 0:
+            return ""
         rect = []
         for i in range(self.height):
             for j in range(self.width):
