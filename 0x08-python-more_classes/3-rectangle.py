0f37a41f95ba438b42f177985fcc2b7e850acbdc
harith01 <harithyusuph@gmail.com>
2023-04-10 13:42:54 +0000

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
             return ""
         rect = []
         for i in range(self.height):
-            for j in range(self.width):
-                rect.append('#')
-            rect.append('\n')
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
         return "".join(rect)
