14ca6bae520cfb36146dabb58caa9a027c0bf9a9
harith01 <harithyusuph@gmail.com>
2023-04-13 09:53:59 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that returns the list of available
+attributes and methods"""
+
+
+def lookup(obj):
+    """Funtion that returns the list of availbale
+    attributes and methods"""
+
+    return dir(obj)
