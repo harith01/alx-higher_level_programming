4532745bbfe956f907df9f348a291cb2b3db1ca3
harith01 <harithyusuph@gmail.com>
2023-04-14 21:03:23 +0000

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,44 +1,7 @@
 #!/usr/bin/python3
 """Defines a BaseGeometry Class"""
 
-
-class BaseGeometry:
-    """Defines a BaseGeometry Class"""
-
-    def area(self):
-        """Computes the area"""
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates an Integer value"""
-
-        if type(value).__name__ != 'int':
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Defines a Rectangle"""
-
-    def __init__(self, width, height):
-        """Initializes an instance of Rectangle"""
-
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """Computes the area of rectangle"""
-
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns string when print()"""
-
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Defines a Square"""
