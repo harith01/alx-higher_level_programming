d2800997ba249a3ce6bd6820edca3de7e8e8ade5
harith01 <harithyusuph@gmail.com>
2023-05-06 15:58:13 +0000

    Create Rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Defines the Rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Creates a representationof a rectangle
+
+    Private instance attributes:
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes an instance of Rectangle"""
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle to value"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle to value"""
+        self.__width = value
+
+    @property
+    def x(self):
+        """Get the x coordinate of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coordinate of the rectangle to value"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y coordinate of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coordinate of the rectangle to value"""
+        self.__y = value
