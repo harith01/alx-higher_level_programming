61029c3d4a2f2c6fc4e7f746b2f575ef78f6c540
harith01 <harithyusuph@gmail.com>
2023-05-07 14:24:08 +0000

    Create Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines a Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize an instance of Square
+
+        Args:
+            size (int): The size of the square
+            x (int): The x coordinate of the square
+            y (int): The y coordinate of the square
+            id (int): The ID of the square
+        """
+
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Override __str___"""
+
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.height)
