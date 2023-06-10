35ef00924a424a71c900e94d4a2c9c37cbba95b9
harith01 <harithyusuph@gmail.com>
2023-06-10 17:49:40 +0000

    Dictionary rep for Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -60,3 +60,12 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Returns dictionary representation of Square"""
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
