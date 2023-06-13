66c71296c32c7bb9963bacd5f61275a9785bb3d3
harith01 <harithyusuph@gmail.com>
2023-06-13 20:10:18 +0000

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 
-console.log(`My number: ${parseInt(process.argv[2])}`);
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`)
