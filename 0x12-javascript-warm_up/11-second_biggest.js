807d5ee05767fcbfeb2590db497599d965ffe7cf
harith01 <harithyusuph@gmail.com>
2023-06-13 21:45:16 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,7 @@
 
 if (process.argv.length < 4) {
   console.log(0);
+} else {
+  const myarr = process.argv.map(Number).slice(2).sort((a, b) => a - b);
+  console.log(myarr[myarr.length - 2]);
 }
-const myarr = process.argv.map(Number).slice(2).sort((a, b) => a - b);
-console.log(myarr[myarr.length - 2]);
