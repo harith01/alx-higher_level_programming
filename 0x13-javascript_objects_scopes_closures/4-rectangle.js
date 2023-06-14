f98c18758e4ac154bd27f19bea4756c73fa6518a
harith01 <harithyusuph@gmail.com>
2023-06-14 16:29:50 +0000

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const myX = [];
+    for (let i = 0; i < this.width; i++) {
+      myX.push('X');
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(myX.join(''));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
