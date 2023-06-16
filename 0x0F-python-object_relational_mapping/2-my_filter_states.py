5a0859ab734ce0715b51742de8128992173c58de
harith01 <harithyusuph@gmail.com>
2023-06-16 14:26:17 +0000

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that takes in an argument
+   and displays all values in the states table
+   from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}' ORDER BY id ASC".format(sys.argv[4])
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
